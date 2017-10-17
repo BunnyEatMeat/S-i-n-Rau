fe0807644847c858409bf21fbcd1435cff7eeaa7
DESKTOP-KKHKH84\Rio.Rai <hixbunny02@gmail.com>
2017-10-17 20:09:19 +0700

    upKhachHang

---
+++ Caffe_Manager_software/Caffe_Manager_software/KhachHang.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Caffe_Manager_software
+{
+    class KhachHang
+    {
+        protected int maKH;
+        protected string tenKH;
+        protected string sdtKH;
+        protected int soLyDaUong;
+
+        public KhachHang()
+        {
+            maKH = 0;
+            tenKH = null;
+            sdtKH = null;
+            soLyDaUong = 0;
+        }
+
+        public KhachHang(int maKhachHang, string tenKhachHang, string soDienThoai, int soLy)
+        {
+            this.maKH = maKhachHang;
+            this.tenKH = tenKhachHang;
+            this.sdtKH = soDienThoai;
+            this.soLyDaUong = soLy;
+        }
+
+        ~KhachHang() { }
+    }
+}
